0e6e9ea272f53ba9d43bb1dcf9760b506b3bf549
Jay Zhang <wangyoucao577@gmail.com>
2020-03-08 16:06:50 +0800

    customizable go version

--- setup-go.sh
+++ setup-go.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -eux
 
-wget --progress=dot:mega https://dl.google.com/go/go1.14.linux-amd64.tar.gz -O go-linux-amd64.tar.gz 
-tar -zxf go-linux-amd64.tar.gz
+GO_LINUX_PACKAGE_URL="https://dl.google.com/go/go1.14.linux-amd64.tar.gz"
+if [ ${INPUT_GOVERSION} == "1.13" ]; then
+    GO_LINUX_PACKAGE_URL="https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz"
+elif [[ ${INPUT_GOVERSION} == "http*" ]]; then
+    GO_LINUX_PACKAGE_URL=${INPUT_GOVERSION}
+fi
+
+wget --progress=dot:mega ${GO_LINUX_PACKAGE_URL} -O go-linux.tar.gz 
+tar -zxf go-linux.tar.gz
 mv go /usr/local/
 mkdir -p /go/bin /go/src /go/pkg
 
