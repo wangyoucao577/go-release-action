e38dd79ba33f081d58f1c55b77a50ed7499550e6
Jay Zhang <wangyoucao577@gmail.com>
2020-03-08 13:22:09 +0800

    fix env name

--- setup-go.sh
+++ setup-go.sh
@@ -5,7 +5,7 @@ tar -zxf go-linux-amd64.tar.gz
 mv go /usr/local/
 mkdir -p /go/bin /go/src /go/pkg
 
-export GOHOME=/usr/local/go
+export GO_HOME=/usr/local/go
 export PATH=${GO_HOME}/bin/:$PATH
 export GOPATH=/go
 
