c7251a1ce196c00cf8363d8da5d823d3a6113efa
Jay Zhang <wangyoucao577@gmail.com>
2021-04-01 00:42:04 +0000

    feat: upgrade default go to 1.16.2, and support 1.14 and 1.15 shortcuts

--- setup-go.sh
+++ setup-go.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -eux
 
-GO_LINUX_PACKAGE_URL="https://dl.google.com/go/go1.14.linux-amd64.tar.gz"
-if [[ ${INPUT_GOVERSION} == "1.13" ]]; then
+GO_LINUX_PACKAGE_URL="https://golang.org/dl/go1.16.2.linux-amd64.tar.gz"
+if [[ ${INPUT_GOVERSION} == "1.15" ]]; then
+    GO_LINUX_PACKAGE_URL="https://golang.org/dl/go1.15.10.linux-amd64.tar.gz"
+elif [[ ${INPUT_GOVERSION} == "1.14" ]]; then
+    GO_LINUX_PACKAGE_URL="https://golang.org/dl/go1.14.15.linux-amd64.tar.gz"
+elif [[ ${INPUT_GOVERSION} == "1.13" ]]; then
     GO_LINUX_PACKAGE_URL="https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz"
 elif [[ ${INPUT_GOVERSION} == http* ]]; then
     GO_LINUX_PACKAGE_URL=${INPUT_GOVERSION}
