9ac77b315b8a60a2953a08ad7e0cfb157429f187
SystemFiles <bensykes12@gmail.com>
2022-04-13 10:35:53 -0400

    BENS-0001 changed package url to go.dev

--- setup-go.sh
+++ setup-go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
-GO_LINUX_PACKAGE_URL="https://dl.google.com/go/$(curl https://go.dev/VERSION?m=text).linux-amd64.tar.gz"
+GO_LINUX_PACKAGE_URL="https://go.dev/dl/$(curl https://go.dev/VERSION?m=text).linux-amd64.tar.gz"
 if [[ ${INPUT_GOVERSION} == "1.17" ]]; then
     GO_LINUX_PACKAGE_URL="https://go.dev/dl/go1.17.linux-amd64.tar.gz"
 elif [[ ${INPUT_GOVERSION} == "1.16" ]]; then
