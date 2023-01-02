1a19f2d6530e734c27ae08754e165accaa31de0e
Jay Zhang <wangyoucao577@gmail.com>
2023-01-02 08:17:00 +0000

    fix: path to array

--- release.sh
+++ release.sh
@@ -76,10 +76,11 @@ mkdir -p ${BUILD_ARTIFACTS_FOLDER}
 
 # build
 INPUT_PROJECT_PATH_IS_ARRAY=FALSE
-if [[ "$( declare -p INPUT_PROJECT_PATH )" =~ "declare -a" ]]; then
+INPUT_PROJECT_PATH_AS_ARRAY=(${INPUT_PROJECT_PATH})
+if [[ "$( declare -p INPUT_PROJECT_PATH_AS_ARRAY )" =~ "declare -a" ]]; then
     INPUT_PROJECT_PATH_IS_ARRAY=TRUE
 fi
-for CURR_PROJECT_PATH in "${INPUT_PROJECT_PATH[@]}"; do
+for CURR_PROJECT_PATH in "${INPUT_PROJECT_PATH_AS_ARRAY[@]}"; do
     cd ${CURR_PROJECT_PATH}
 
     # for array project paths, use each project path's name as binary name
